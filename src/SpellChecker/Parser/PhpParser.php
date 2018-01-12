8962e07eea89869bdb0faa7a08e2b24caae27b16
Vlasta Neubauer <paranoiq@centrum.cz>
2018-01-12 12:50:11 +0100

    Add PhpParser

---
+++ src/SpellChecker/Parser/PhpParser.php
@@ -0,0 +1,128 @@
+<?php declare(strict_types = 1);
+
+namespace SpellChecker\Parser;
+
+use PhpParser\Parser\Tokens;
+
+class PhpParser implements \SpellChecker\Parser\Parser
+{
+
+    public const CONTEXT_CODE = 'code';
+    public const CONTEXT_STRING = 'string';
+    public const CONTEXT_COMMENT = 'comment';
+    public const CONTEXT_DOC = 'doc';
+    public const CONTEXT_HTML = 'html';
+    public const CONTEXT_DATA = 'data';
+
+    /** @var \SpellChecker\Parser\PhpLexer */
+    private $phpLexer;
+
+    /** @var \SpellChecker\Parser\DefaultParser */
+    private $defaultParser;
+
+    public function __construct(DefaultParser $defaultParser)
+    {
+        $this->defaultParser = $defaultParser;
+        $this->phpLexer = new PhpLexer([
+            'usedAttributes' => ['startLine', 'startFilePos'],
+        ]);
+    }
+
+    /**
+     * @param string $string
+     * @return string[]
+     */
+    public function parse(string $string): array
+    {
+        $results = [];
+        $lastHeredoc = null;
+        $this->phpLexer->startLexing($string);
+        while ($token = $this->phpLexer->getNextToken($value, $startAttributes, $endAttributes)) {
+            if ($token < 256) {
+                continue;
+            }
+            $rowNumber = $startAttributes['startLine'];
+            $position = $startAttributes['startFilePos'];
+
+            switch ($token) {
+                case Tokens::T_COMMENT:
+                    // // or #, and /* */
+                    $this->parseString($results, $value, $position, $rowNumber, self::CONTEXT_COMMENT);
+                    break;
+                case Tokens::T_DOC_COMMENT:
+                    // /** */
+                    $this->parseString($results, $value, $position, $rowNumber, self::CONTEXT_DOC);
+                    break;
+                case Tokens::T_CONSTANT_ENCAPSED_STRING:
+                    // "foo" or 'bar'
+                    $this->parseString($results, $value, $position, $rowNumber, self::CONTEXT_STRING);
+                    break;
+                case Tokens::T_START_HEREDOC:
+                    // <<<FOO
+                    $lastHeredoc = trim(substr($value, 3), "'\"\n");
+                    $position += strlen(rtrim($value, "'\"\n")) - strlen($lastHeredoc);
+                    $this->defaultParser->blocksToWords($lastHeredoc, $position, $rowNumber, $results, self::CONTEXT_CODE);
+                    break;
+                case Tokens::T_ENCAPSED_AND_WHITESPACE:
+                    // parts of heredoc or string with variables
+                    if ($lastHeredoc !== null && substr($value, -strlen($lastHeredoc) - 1) === $lastHeredoc . ';') {
+                        $value = substr($value, 0, -strlen($lastHeredoc) - 1);
+                        $this->parseString($results, $value, $position, $rowNumber, self::CONTEXT_STRING);
+                        $offset = strlen($value);
+                        $rows = strlen($value) - strlen(str_replace("\n", '', $value));
+                        $this->defaultParser->blocksToWords($lastHeredoc, $position + $offset, $rowNumber + $rows, $results, self::CONTEXT_CODE);
+                        $lastHeredoc = null;
+                    } else {
+                        $this->parseString($results, $value, $position, $rowNumber, self::CONTEXT_STRING);
+                    }
+                    break;
+                case Tokens::T_STRING:
+                    // identifiers, e.g. keywords like parent and self, function names, class names and more
+                    $this->defaultParser->blocksToWords($value, $position, $rowNumber, $results, self::CONTEXT_CODE);
+                    break;
+                case Tokens::T_VARIABLE:
+                    // $foo
+                    $nameValue = ltrim($value, '$');
+                    $position += strlen($value) - strlen($nameValue);
+                    $this->defaultParser->blocksToWords($nameValue, $position, $rowNumber, $results, self::CONTEXT_CODE);
+                    break;
+                case Tokens::T_STRING_VARNAME:
+                    $this->defaultParser->blocksToWords($value, $position, $rowNumber, $results, self::CONTEXT_CODE);
+                    break;
+                case Tokens::T_INLINE_HTML:
+                    // any text outside <?php
+                    $this->parseString($results, $value, $position, $rowNumber, self::CONTEXT_HTML);
+                    break;
+                case Tokens::T_HALT_COMPILER:
+                    // anything after __halt_compiler(); instructions
+                    $after = $this->phpLexer->handleHaltCompiler();
+                    $this->parseString($results, $after, $position + 18, $rowNumber, self::CONTEXT_DATA);
+                    break;
+            }
+        }
+
+        return $results;
+    }
+
+    private function parseString(array &$result, string $string, int $filePosition, int $rowNumber, string $context): void
+    {
+        $rowOffset = 0;
+        foreach (explode("\n", $string) as $rowIndex => $row) {
+            if (!preg_match_all(DefaultParser::WORD_BLOCK_REGEXP, $row, $blockMatches, PREG_OFFSET_CAPTURE)) {
+                $rowOffset += strlen($row) + 1;
+                continue;
+            }
+            foreach ($blockMatches[0] as [$block, $rowPosition]) {
+                $this->defaultParser->blocksToWords(
+                    $block,
+                    $filePosition + $rowOffset + $rowPosition,
+                    $rowNumber + $rowIndex,
+                    $result,
+                    $context
+                );
+            }
+            $rowOffset += strlen($row) + 1;
+        }
+    }
+
+}
---
+++ tests/SpellChecker/Parser/PhpParser.phpt
@@ -0,0 +1,176 @@
+<?php declare(strict_types = 1);
+
+namespace SpellChecker\Parser;
+
+use SpellChecker\Word;
+use Tester\Assert;
+
+require '../../bootstrap.php';
+
+$defaultParser = new DefaultParser();
+$phpParser = new PhpParser($defaultParser);
+
+
+$names = '<?php declare(aaa = 1)
+namespace bbb
+class ccc {
+    const ddd = 0;
+    private \$eee;
+    function fff(ggg \$hhh): iii {
+        jjj();
+        \$kkk = \$\$lll;
+    }
+}';
+$actual = $phpParser->parse($names);
+$expected = [
+    new Word('aaa', null,  14, 1, PhpParser::CONTEXT_CODE),
+    new Word('bbb', null,  33, 2, PhpParser::CONTEXT_CODE),
+    new Word('ccc', null,  43, 3, PhpParser::CONTEXT_CODE),
+    new Word('ddd', null,  59, 4, PhpParser::CONTEXT_CODE),
+    new Word('eee', null,  82, 5, PhpParser::CONTEXT_CODE),
+    new Word('fff', null, 100, 6, PhpParser::CONTEXT_CODE),
+    new Word('ggg', null, 104, 6, PhpParser::CONTEXT_CODE),
+    new Word('hhh', null, 110, 6, PhpParser::CONTEXT_CODE),
+    new Word('iii', null, 116, 6, PhpParser::CONTEXT_CODE),
+    new Word('jjj', null, 130, 7, PhpParser::CONTEXT_CODE),
+    new Word('kkk', null, 147, 8, PhpParser::CONTEXT_CODE),
+    new Word('lll', null, 157, 8, PhpParser::CONTEXT_CODE),
+];
+Assert::equal($expected, $actual);
+
+
+$string = '<?php
+$aaa = "bbb
+ccc";';
+$expected = [
+    new Word('aaa', null,  7, 2, PhpParser::CONTEXT_CODE),
+    new Word('bbb', null, 14, 2, PhpParser::CONTEXT_STRING),
+    new Word('ccc', null, 18, 3, PhpParser::CONTEXT_STRING),
+];
+$actual = $phpParser->parse($string);
+Assert::equal($expected, $actual);
+
+
+$string = '<?php
+$aaa = \'bbb
+ccc\'';
+$expected = [
+    new Word('aaa', null,  7, 2, PhpParser::CONTEXT_CODE),
+    new Word('bbb', null, 14, 2, PhpParser::CONTEXT_STRING),
+    new Word('ccc', null, 18, 3, PhpParser::CONTEXT_STRING),
+];
+$actual = $phpParser->parse($string);
+Assert::equal($expected, $actual);
+
+
+$string = '<?php
+$aaa = <<<EOT
+bbb
+ccc
+EOT;';
+$expected = [
+    new Word('aaa', null,  7, 2, PhpParser::CONTEXT_CODE),
+    new Word('EOT', null, 16, 2, PhpParser::CONTEXT_CODE),
+    new Word('bbb', null, 20, 3, PhpParser::CONTEXT_STRING),
+    new Word('ccc', null, 24, 4, PhpParser::CONTEXT_STRING),
+    new Word('EOT', null, 28, 5, PhpParser::CONTEXT_CODE),
+];
+$actual = $phpParser->parse($string);
+Assert::equal($expected, $actual);
+
+
+$string = '<?php
+$aaa = <<<\'EOT\'
+bbb
+ccc
+EOT;';
+$expected = [
+    new Word('aaa', null,  7, 2, PhpParser::CONTEXT_CODE),
+    new Word('EOT', null, 17, 2, PhpParser::CONTEXT_CODE),
+    new Word('bbb', null, 22, 3, PhpParser::CONTEXT_STRING),
+    new Word('ccc', null, 26, 4, PhpParser::CONTEXT_STRING),
+    new Word('EOT', null, 30, 5, PhpParser::CONTEXT_CODE),
+];
+$actual = $phpParser->parse($string);
+Assert::equal($expected, $actual);
+
+
+$string = '<?php
+$aaa = "bbb $ccc ddd";';
+$expected = [
+    new Word('aaa', null,  7, 2, PhpParser::CONTEXT_CODE),
+    new Word('bbb', null, 14, 2, PhpParser::CONTEXT_STRING),
+    new Word('ccc', null, 19, 2, PhpParser::CONTEXT_CODE),
+    new Word('ddd', null, 23, 2, PhpParser::CONTEXT_STRING),
+];
+$actual = $phpParser->parse($string);
+Assert::equal($expected, $actual);
+
+
+$string = '<?php
+$aaa = "bbb {$ccc} ddd";';
+$expected = [
+    new Word('aaa', null,  7, 2, PhpParser::CONTEXT_CODE),
+    new Word('bbb', null, 14, 2, PhpParser::CONTEXT_STRING),
+    new Word('ccc', null, 20, 2, PhpParser::CONTEXT_CODE),
+    new Word('ddd', null, 25, 2, PhpParser::CONTEXT_STRING),
+];
+$actual = $phpParser->parse($string);
+Assert::equal($expected, $actual);
+
+
+$string = '<?php
+$aaa = "bbb {$ccc->ddd} eee";';
+$expected = [
+    new Word('aaa', null,  7, 2, PhpParser::CONTEXT_CODE),
+    new Word('bbb', null, 14, 2, PhpParser::CONTEXT_STRING),
+    new Word('ccc', null, 20, 2, PhpParser::CONTEXT_CODE),
+    new Word('ddd', null, 25, 2, PhpParser::CONTEXT_CODE),
+    new Word('eee', null, 30, 2, PhpParser::CONTEXT_STRING),
+];
+$actual = $phpParser->parse($string);
+Assert::equal($expected, $actual);
+
+
+$string = '<?php
+$aaa = "bbb {$ccc[\'ddd\']} eee"';
+$expected = [
+    new Word('aaa', null,  7, 2, PhpParser::CONTEXT_CODE),
+    new Word('bbb', null, 14, 2, PhpParser::CONTEXT_STRING),
+    new Word('ccc', null, 20, 2, PhpParser::CONTEXT_CODE),
+    new Word('ddd', null, 25, 2, PhpParser::CONTEXT_STRING),
+    new Word('eee', null, 32, 2, PhpParser::CONTEXT_STRING),
+];
+$actual = $phpParser->parse($string);
+Assert::equal($expected, $actual);
+
+
+$html = '
+aaa
+bbb
+<?php
+$ccc = 1;
+?>
+ddd
+eee';
+$expected = [
+    new Word('aaa', null,  1, 2, PhpParser::CONTEXT_HTML),
+    new Word('bbb', null,  5, 3, PhpParser::CONTEXT_HTML),
+    new Word('ccc', null, 16, 5, PhpParser::CONTEXT_CODE),
+    new Word('ddd', null, 28, 7, PhpParser::CONTEXT_HTML),
+    new Word('eee', null, 32, 8, PhpParser::CONTEXT_HTML),
+];
+$actual = $phpParser->parse($html);
+Assert::equal($expected, $actual);
+
+
+$halt = '<?php
+__halt_compiler();
+aaa
+bbb';
+$expected = [
+    new Word('aaa', null, 25, 3, PhpParser::CONTEXT_DATA),
+    new Word('bbb', null, 29, 4, PhpParser::CONTEXT_DATA),
+];
+$actual = $phpParser->parse($halt);
+Assert::equal($expected, $actual);
